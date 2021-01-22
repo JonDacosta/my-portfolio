d66693b7dd25e243879d9a237ea750059ac0e0ed
Jonatan Dacosta <dacostaafonso@gmail.com>
2021-01-22 01:29:16 +0100

    fix form 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
     
 
     const { name, email, message } = formState;
-    const form = getElementById("form");
+    
     useEffect( () => {
         console.log('email cambió')
     }, [ email ]);
